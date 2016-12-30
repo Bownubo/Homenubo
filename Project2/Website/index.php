a7d98a5789d7c42f51e38f0a648820a65118ff57
Bownubo <stanw.vandenberg@gmail.com>
2016-12-30 16:31:39 +0100

    website updated to about

--- Project2/Website/index.php
+++ Project2/Website/index.php
@@ -1,34 +1,61 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<html lang="nl">
 <head>
 <meta charset="utf-8">
 <meta name="description" content="Chillie's Galore">
 <meta name="Author" content="Stan van den berg">
-<link rel="stylesheet" type="text/css" href="main.css">
 <meta name="vieuwport" content="width=device-width , initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="main.css">
 <script src="https://code.jquery.com/jquery-3.1.1.js"></script>
 <script src="./js/main.js"></script>
 <title>Chillie's Galore</title>
 </head>
 <body>
-<header class="clearHeader">
-  <nav>
-    <a href="#Home">Home</a>
-    <a href="#About">About</a>
-    <a href="#Menu">Menu</a>
-    <a href="#Gallery">Gallery</a>
-    <a href="#Catering">Catering</a>
-    <a href="#contact">Contact</a>
-</nav>
-</header>
+  <header class="clearHeader">
+    <nav>
+      <a href="#Home">Home</a>
+      <a href="#About">About</a>
+      <a href="#Menu">Menu</a>
+      <a href="#Gallery">Gallery</a>
+      <a href="#Catering">Catering</a>
+      <a href="#Contact">Contact</a>
+    </nav>
+  </header>
   <img src="img/background4.jpg" alt="Paniek" id="background">
-  <img src="img/chili_calorne.png" id="logobig">
-  <img src="img/arrow.png" id="arrow">
-<div class="wrapper">
-</div>
-<section class="text">
-<p>hello world</p>
-</section>
+
+  <div id="padding">
+    <section id="title">
+      <img src="img/chili_calorne.png" id="logobig">
+      <a href=#Home><img src="img/arrow.png" id="arrow"></a>
+    </section>
+
+    <section id="Home">
+      <div class="textcontainer">
+        <h1>Home</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ullamcorper enim orci, at varius nulla accumsan sit amet. Morbi pretium scelerisque sem, at convallis elit volutpat sit amet. Suspendisse potenti. Donec vitae molestie dui. Vestibulum vitae nisl eu tellus facilisis ornare et vitae ex. Nullam a nunc dignissim, placerat erat eu, facilisis justo. Curabitur et diam diam.</p>
+        <p>Suspendisse vehicula tortor nec vulputate suscipit. Phasellus pellentesque velit est, et tincidunt enim tristique eu. Proin aliquam purus a elementum eleifend. Aenean bibendum sapien sit amet quam tristique egestas. Nullam tempus luctus nibh vel interdum. Aliquam rutrum mi sed ante dictum, dictum gravida urna rutrum. Sed tristique consequat leo, eu vulputate sapien. Donec a neque gravida, rutrum tortor ut, gravida felis. Vivamus dictum, mauris sed egestas hendrerit, lectus nibh rutrum neque, a lacinia mi tortor at justo. Nullam gravida felis odio, eu imperdiet erat hendrerit non. Pellentesque faucibus rhoncus diam, at euismod ligula porttitor quis. Duis a consectetur nisl. Nam sed risus lobortis, faucibus elit in, sagittis augue. Nam eu convallis risus. Ut ultrices non ante elementum pretium. Ut sed ligula sem.</p>
+        <p>Nullam mauris ligula, lobortis vitae erat sit amet, efficitur pharetra elit. Ut id diam eget metus efficitur condimentum. Integer velit massa, faucibus sit amet placerat at, aliquet et turpis. Nunc mi libero, sagittis sit amet tortor at, consequat fringilla felis. Proin commodo metus risus. Cras pellentesque enim at libero euismod, a dignissim lacus tristique. Integer ut eros erat.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lectus neque, interdum non faucibus pellentesque, tincidunt a ex. Nunc dignissim lobortis ante, sit amet iaculis libero. Nulla vel enim eu nulla aliquam convallis. Pellentesque cursus euismod sapien. Fusce fermentum scelerisque elit ut faucibus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce iaculis elementum lacus, sit amet venenatis elit varius sed. Cras dolor lacus, ultrices id tellus non, rutrum mattis lorem. Pellentesque ornare volutpat gravida.</p>
+
+      </div>
+    </section>
+
+    <section id="About">
+      <div class="textcontainer">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/TGY3iP4X34c" frameborder="0" allowfullscreen></iframe>
+        <h1>About</h1>
+        <p>Suspendisse potenti. Suspendisse potenti. Aliquam pellentesque sollicitudin neque vel cursus. Nulla vulputate consectetur ex a finibus. Maecenas bibendum pulvinar libero eu tristique. Curabitur vitae diam vulputate, scelerisque elit eu, volutpat risus. Fusce congue lectus non felis dapibus, sed tincidunt metus luctus. Vivamus nisl sapien, sodales vitae dignissim eget, eleifend vel elit. Integer lobortis egestas iaculis. Pellentesque ac blandit neque, pellentesque vulputate eros. Nunc pharetra vestibulum felis, sed rhoncus tortor auctor sed. Quisque viverra dui eu justo vulputate, in egestas urna ultricies. Maecenas luctus enim lorem, vitae tristique ligula fermentum sit amet. Nunc rhoncus, elit luctus lacinia pretium, lectus mauris convallis elit, id gravida dui enim in lorem.</p>
+        <p>Nam vitae sem lacinia, semper tortor vel, auctor elit. Etiam sed nulla non mi venenatis consequat in non mi. Nullam euismod pulvinar volutpat. Pellentesque molestie orci odio, a placerat turpis semper et. Suspendisse pulvinar sem ac nibh consequat porttitor. Nam molestie ullamcorper laoreet. Maecenas et purus quis nibh fermentum imperdiet. Vivamus varius risus vitae sodales egestas. Aliquam porta efficitur lacus at viverra. Curabitur sit amet sem quis ipsum dignissim consequat eget at ipsum. Mauris commodo pulvinar sapien, a vehicula neque. Morbi at fringilla erat. Cras porttitor et nibh bibendum venenatis.</p>
+        <img src="img/food1.jpg" alt="food1error" id="food1">
+        <img src="img/food2.jpg" alt="food2error" id="food2">
+
+        </svg>
+      </div>
+    </section>
+  </div>
 </body>
-</HTML>
+
+
+
+
+</html>
